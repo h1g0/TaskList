7128574d373c47710cb6d00f9c6bc1be7f6ad3d2
hi_go <me@clutte.red>
2019-10-26 04:21:50 +0900

    implement multi-column ( very buggy ).

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,33 @@
     <div class="header">
       <h1>TaskList</h1>
       <div class="header-menu"> 
-        <button class="header-menu-button" @click="showOutput()"> <img src="img/plus.svg" title="カラムを追加"/></button>
+        <button class="header-menu-button" @click="addColumn()"> <img src="img/plus.svg" title="カラムを追加"/></button>
         <button class="header-menu-button" @click="showOutput()"> <img src="img/note-text.svg" title="出力画面を表示/非表示"/></button>
         <button class="header-menu-button" @click="showSettings()" ><img src="img/settings.svg" title="設定画面を表示/非表示"/></button>
       </div>
     </div>
+    <draggable
+      v-model="taskColumnList"
+      v-bind="dragOptions"
+      group="column"
+      @start="drag=true"
+      @end="drag=false"
+      class="task-colmun-container">
+      <task-column 
+        v-for="(column,index) in taskColumnList"
+        v-bind:taskList="column.taskList"
+        v-bind:index="index"
+        v-bind:totalsize="taskColumnList.length"
+        v-bind:key="column.id"
+        :ref="index"
+        @add-column="addColumn(index)"
+        @delete-colmn="deleteColumn(index)"
+        @save-item="saveItem()"
+      ></task-column>
+    </draggable>
 
+
+<!--
     <div class="main-content">
       <div class="tasks">
         <draggable
@@ -35,7 +56,7 @@
         </draggable>
       </div>
     </div>
-
+-->
     <div class="output" v-show="taskListSettings.isOutputVisible">
       <input type="button" value="出力" class="menu" @click="outputResult" title="テキストとして出力" />
       <input type="button" value="コピー" class="menu" @click="copyResult" title="出力結果をクリップボードにコピー"/>
@@ -73,19 +94,19 @@
 </template>
 
 <script>
-import TaskItem from "./components/TaskItem.vue";
+import TaskColumn from "./components/TaskColumn.vue";
 import draggable from 'vuedraggable'
 
 export default {
   name: "app",
   components: {
-    TaskItem,
+    TaskColumn,
     draggable
   },
   directives: {},
   data: function() {
     return {
-      taskList: [this.buildNewItem()],
+      taskColumnList: [this.buildNewColumn()],
       outputStr: "",
       taskListOutputTemplate: {
         header: "## TODO{br}{br}",
@@ -118,8 +139,24 @@ export default {
         return ((Math.random() * 16) | 0).toString(16);
       });
     },
-    buildNewItem: function() {
-      return { id: this.buildId(), indent: 0, checked: false, text: "" };
+    buildNewColumn: function() {
+      return {
+        id: this.buildId(), 
+        title: "",
+        taskList: [],
+      };
+    },
+    addColumn: function(index) {
+      this.taskColumnList.splice(index + 1, 0, this.buildNewColumn());
+      //this.taskList[index + 1].indent = this.taskList[index].indent;
+      this.saveItem();
+    },
+    deleteColumn: function(index) {
+      if (!window.confirm("カラムを削除します。よろしいですか？")) {
+        return;
+      }
+      this.taskColumnList.splice(index, 1);
+      this.saveItem();
     },
     showOutput:function(){
       (this.taskListSettings.isOutputVisible)?
@@ -140,28 +177,8 @@ export default {
         }
       }
     },
-    addItem: function(index) {
-      this.taskList.splice(index + 1, 0, this.buildNewItem());
-      this.taskList[index + 1].indent = this.taskList[index].indent;
-      this.saveItem();
-    },
-    replaceItem: function(upperIndex, lowerIndex) {
-      var temp = this.taskList[lowerIndex];
-      this.taskList.splice(lowerIndex, 1, this.taskList[upperIndex]);
-      this.taskList.splice(upperIndex, 1, temp);
-    },
-    deleteItem: function(index) {
-      if (!this.$refs[index]["0"].isDeletableItem) {
-        return;
-      }
-      if (!window.confirm("アイテムを削除します。よろしいですか？")) {
-        return;
-      }
-      this.taskList.splice(index, 1);
-      this.saveItem();
-    },
     saveItem: function() {
-      localStorage.taskList = JSON.stringify(this.taskList);
+      localStorage.taskColumnList = JSON.stringify(this.taskColumnList);
     },
     saveSettings: function(){
       localStorage.taskListOutputTemplate = JSON.stringify(this.taskListOutputTemplate);
@@ -197,7 +214,7 @@ export default {
   },
   mounted() {
     if (localStorage.taskList) {
-      this.taskList = JSON.parse(localStorage.taskList);
+      this.taskColumnList = JSON.parse(localStorage.taskColumnList);
     }
     if (localStorage.taskListOutputTemplate) {
       this.taskListOutputTemplate = JSON.parse(localStorage.taskListOutputTemplate);
@@ -256,13 +273,14 @@ html{
 body{
   margin:0px;
   height: 100%;
-  background-color:#414345;
+  background: linear-gradient(to top, #414345, #232526);
+  background-attachment: fixed;
 }
 #app{
   margin: 0px;
   padding: 0px;
   height: 100%;
-  background: linear-gradient(to top, #414345, #232526);
+
 }
 .header{
   position: fixed;
@@ -279,9 +297,13 @@ body{
   height: 60px;
 }
 .header-menu{
-  margin-right: 30px;
+  margin-right: 10px;
   display: flex;
   align-items: center;
+}
+.header-dummy{
+  width:100%;
+  height:60px;
 }
 .sidebar{
   position: fixed;
@@ -293,18 +315,14 @@ body{
   height:100%;
   background: linear-gradient(to left, #ece9e6, #ffffff);
 }
-.main-content{
-  position: relative;
-  margin-left: auto;
-  margin-right: auto;
-  z-index: 1;
-  padding: 0px 20px 0px 20px;
-  background-color: #ffffff;
-  box-shadow: -8px 0px 8px 0px rgba(0,0,0,0.3), 8px 0px 8px 0px rgba(0,0,0,0.3);
-  width: 800px;
-  min-height: 100%;
+.task-colmun-container{
+  width:auto;
+  height: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: stretch;
+  overflow-x: scroll;
 }
-
 .tasks{
   padding-top: 120px;
   margin-bottom: 60px;
@@ -348,7 +366,7 @@ h1 {
   color: rgba(0,0,0,0.6);
   text-shadow: 2px 2px 3px rgba(255,255,255,0.1);
   display: inline-block;
-  padding-left:30px;
+  padding-left:10px;
 }
 h2 {
   font-size: 16px;
--- src/components/TaskColumn.vue
+++ src/components/TaskColumn.vue
@@ -1,7 +1,132 @@
 <template>
-    
+  <div class="column">
+    <div class="tasks">
+      <draggable
+        v-model="taskList"
+        v-bind="dragOptions"
+        group="task"
+        handle=".handle"
+        @start="drag=true"
+        @end="drag=false"
+      >
+      <transition-group type="transition" :name="drag ? 'flip-list' : null">
+        <task-item
+          v-for="(item,index) in taskList"
+          v-bind:task="item"
+          v-bind:index="index"
+          v-bind:totalsize="taskList.length"
+          v-bind:key="item.id"
+          :ref="index"
+          @add-item="addItem()"
+          @delete-item="deleteItem(index)"
+          @save-item="saveItem()"
+        ></task-item>
+        </transition-group>
+      </draggable>
+    </div>
+
+      <button class="header-menu-button" @click="addItem"> <img src="img/plus.svg" title="タスクを追加"/></button>
+  </div>
 </template>
 <script>
+import TaskItem from "./TaskItem.vue";
+import draggable from 'vuedraggable'
+export default {
+  name: "TaskColumn",
+  components: {
+    TaskItem,
+    draggable
+  },
+  props: ["taskList", "index", "totalsize"],
+  directives: {
+    visible: {
+      update: function(el, binding) {
+        el.style.visibility = binding.value ? "visible" : "hidden";
+      }
+    }
+  },
+  data: function() {
+    return {
+      indentWidth: 30,
+      isTopItem: true,
+      isBottomItem: true,
+      isDeletableItem: false,
+      isEditing: false,
+      isShowItemButtons: false,
+    };
+  },
+  computed: {
+    dragOptions() {
+      return {
+        animation: 200,
+        group: "task",
+        disabled: false,
+        ghostClass: "ghost"
+      };
+    }
+  },
+  methods:{
+    buildId: function() {
+      return "xxxxx".replace(/x/g, function() {
+        return ((Math.random() * 16) | 0).toString(16);
+      });
+    },
+    buildNewItem: function() {
+      return { id: this.buildId(), indent: 0, checked: false, text: "" };
+    },
+    addItem: function() {
+      
+      this.taskList.splice(this.taskList.length, 0, this.buildNewItem());
+      //this.taskList[index + 1].indent = this.taskList[index].indent;
+      this.saveItem();
+    },
+    replaceItem: function(upperIndex, lowerIndex) {
+      var temp = this.taskList[lowerIndex];
+      this.taskList.splice(lowerIndex, 1, this.taskList[upperIndex]);
+      this.taskList.splice(upperIndex, 1, temp);
+    },
+    deleteItem: function(index) {
+      if (!this.$refs[index]["0"].isDeletableItem) {
+        return;
+      }
+      if (!window.confirm("アイテムを削除します。よろしいですか？")) {
+        return;
+      }
+      this.taskList.splice(index, 1);
+      this.saveItem();
+    },
+    saveItem:function(){
+      this.$emit('save-item');
+    },
+    setAllItemState: function() {
+      for (var item in this.$refs) {
+        if (this.$refs[item]["0"] != null) {
+          this.$refs[item]["0"].setItemState();
+        }
+      }
+    }
+  },
+  updated: function() {
+    this.$nextTick(function() {
+      this.setAllItemState();
+    });
+    this.saveItem();
+  }
+};
 </script>
-<style>
+<style scoped>
+
+.column{
+  margin-top: 70px;
+  margin-left:10px;
+  z-index: 1;
+  padding: 10px 0px 3px 0px;
+  border-radius: 10px;
+  background-color: #ffffff;
+  box-shadow: -8px 0px 8px 0px rgba(0,0,0,0.3), 8px 0px 8px 0px rgba(0,0,0,0.3);
+  min-width: 350px;
+  height: auto;
+  overflow: hidden;
+}
+
 </style>
